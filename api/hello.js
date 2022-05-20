e296b18162658f49b792d327b8ce9613f30efd2c
Agustin Sobral <asobralr@gmail.com>
2022-05-20 14:00:28 -0300

    Test mail

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,16 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey("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");
+
 module.exports = (req, res) => {
+  const dest = "asobralr@gmail.com"; //data.dest;
+  const msg = {
+    to: dest,
+    from: { name: "Brainbo App", email: "welcome@mycoocoon.app" },
+    fromname: "Brainbo App",
+    subject: "Welcome to brainbo meditation app",
+    text: "Welcome to mycoocoon! Enjoy your personal meditation and unleash the power of light and color to reveal your current needs. Just use your intuition and let mycoocoon do the magic.",
+  };
+  sgMail.send(msg);
   res.json({
     hola: "mundo",
   });
